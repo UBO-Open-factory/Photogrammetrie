82819af5107ce9efb2324edb2b386bb186f17e0b
UltraSjj <107261678+UltraSjj@users.noreply.github.com>
2022-07-06 16:31:59 +0200

    Yii

---
+++ common/config/params-local.php
@@ -0,0 +1,7 @@
+<?php
+
+Yii::setAlias('@webroot',realpath(dirname(__FILE__).'C:\xampp\htdocs\RPI_Photogrammétrie\backend\web'));
+Yii::setAlias('@runtime',realpath(dirname(__FILE__).'C:\xampp\htdocs\RPI_Photogrammétrie\backend\runtime'));
+
+return [
+];
