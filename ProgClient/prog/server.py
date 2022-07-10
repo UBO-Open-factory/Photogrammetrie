bd2f9cafc7cfd03b8f2c655409e62f70670cd2f2
TitouanMelon <108010900+TitouanMelon@users.noreply.github.com>
2022-07-10 16:10:18 +0200

    update server.py

--- ProgClient/prog/server.py
+++ ProgClient/prog/server.py
@@ -12,17 +12,15 @@ and add every message receive in a sql database"""
 
 # Import libraries ----------------------------------------------------------
 import paho.mqtt.client as mqtt
-#pip install mysql-connector-python
 import mysql.connector
 # ---------------------------------------------------------------------------
 
-#global variable-------------------------------------------------------------
-mydb=0
-# ---------------------------------------------------------------------------
-
-
 # const variable ------------------------------------------------------------
 BROKER_IP = "ip.of.the.broker"
+USER_DB = "username"
+PASS_DB = "password"
+IP_DB = "ip.of.the.db"
+DB = "mydb"
 # ---------------------------------------------------------------------------
 
 #Callback function ----------------------------------------------------------
@@ -32,30 +30,50 @@ def on_connect(client, userdata, flags, rc):
 	print ("connected with result code" + str(rc))
 	# Suscribe to a project
 	ourClient.subscribe("Register")
+	ourClient.subscribe("IsAlive")
 
 def messageFunction (client, userdata, message):
 	""" When client receive a message on Register topic
 	the function add the message in database """
-	global mydb
 	topic = str(message.topic).split('/') #We divide the topic by '/' character
 	message = str(message.payload.decode("utf-8"))
-	if (topic[0] == "Register"):
+
+	#Open database
+	mydb = mysql.connector.connect(host = IP_DB, user = USER_DB, password = PASS_DB)
+
+	if (topic[0] == "IsAlive"):
+		#Create cursor to execute SQL command
 		cursor = mydb.cursor()
-		sql = """INSERT INTO MAC(ADDRESS) VALUES ('"""+message+"""')"""
+
+		sql = "update "+DB+" set alive = %s where address = %s";
 		try:
-			cursor.execute(sql)
-			mydb.commit()
+			cursor.execute(sql, ["1", message]) #Add mac address
+			mydb.commit() #Commit
 		except:
-			mydb.rollback()
+			mydb.rollback() #If command fail rollback
+	if (topic[0] == "Register"):
+		#Create cursor to execute SQL command
+		cursor = mydb.cursor()
+		try:
+			sql = "select address from "+DB+" where address=%s;"
+			cursor.execute(sql, [message]) #Verify if mac address does not exist
+			result = cursor.fetchall()
+			if (len(result) == 0):
+				sql = "insert into "+DB+"(address,alive) values (%s,%s);"
+				cursor.execute(sql, [message, "1"]) #Add mac address
+				mydb.commit() #Commit
+				print ("Success")
+			else:
+				print ("Already in the databases")
+		except:
+			mydb.rollback() #If command fail rollback
+			print ("Failure")
+	mydb.close() #Close database
 
 # ---------------------------------------------------------------------------
 
 # main ----------------------------------------------------------------------
-
-# Creating connection object
-mydb = mysql.connector.connect(host = "localhost", user = "yourusername", password = "your_password")
-
-ourClient = mqtt.Client(mac_address[0]) #Create client with mac address for the name
+ourClient = mqtt.Client("Broker") #Create client with mac address for the name
 ourClient.on_connect = on_connect #Define the connect callback for when we are connected
 ourClient.on_message = messageFunction #Define the callback function for when the client receive a message
 
